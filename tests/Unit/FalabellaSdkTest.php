d8b17e6c61d9e2aca19df16ea81e5f33407a5129
Enzo Notario <enzo.notario@astroselling.com>
2024-07-16 13:10:20 -0300

    chore: fix tests

--- tests/Unit/FalabellaSdkTest.php
+++ tests/Unit/FalabellaSdkTest.php
@@ -1,8 +1,14 @@
 <?php
 
 use Astroselling\FalabellaSdk\FalabellaSdk;
+use Astroselling\FalabellaSdk\Models\FalabellaFeed;
 
 it('can be instanced', function () {
-    $sdk = new FalabellaSdk('', '');
+    $sdk = new FalabellaSdk('', '', 'MEX');
     $this->assertInstanceOf(FalabellaSdk::class, $sdk);
+});
+
+it('can create a feed', function () {
+    $f = FalabellaFeed::factory()->create();
+    $this->assertModelExists($f);
 });
